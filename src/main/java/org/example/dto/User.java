3a27dfd217358314c3d46d9277c2b0f94d6c27b5
Nam Thang <thangvynam1808@gmail.com>
2024-03-17 13:21:43 +0700

    initialize

---
+++ src/main/java/org/example/dto/User.java
@@ -0,0 +1,48 @@
+package org.example.dto;
+
+import java.util.Objects;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Age: " + age;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
